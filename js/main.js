a3a4b9e1447c275156116a8167bf770e60a4b733
emre kılıç <emrekilic87@gmail.com>
2020-07-31 02:50:15 +0300

    console logs removed

--- js/main.js
+++ js/main.js
@@ -15,13 +15,10 @@ var vm = new Vue({
             var request = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=' + 'Istanbul,tr' + '&appid=75e134306d5dc062ce7553905b8032aa';
 
             this.$http.get(request).then(function (response) {
-                console.log(response)
                 this.dataObj = response.data;
                 this.city = this.dataObj.city.name;
                 this.weatherList = this.dataObj.list;
-            }, function (response) {
-                console.log(response);
-                });
+            });
         }
     }
     
