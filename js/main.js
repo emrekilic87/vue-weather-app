d463bd64915a04f7e64ea3d52388a8dcc1e180c1
emre kılıç <emrekilic87@gmail.com>
2020-07-31 02:34:19 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,34 @@
+﻿
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        dataObj: null,
+        city: '',
+        weatherList: []
+    },
+    created: function () {
+        this.getWeather();
+    },
+    methods: {
+        getWeather: function () {
+            var request = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + 'Istanbul,tr' + '&appid=75e134306d5dc062ce7553905b8032aa';
+
+            this.$http.get(request).then(function (response) {
+                console.log(response)
+                this.dataObj = response.data;
+                this.city = this.dataObj.city.name;
+                this.weatherList = this.dataObj.list;
+            }, function (response) {
+                console.log(response);
+                });
+        }
+    }
+    
+
+
+})
+
+
+
+
